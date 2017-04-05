61b8ef16ad575ac5ec99a5370ddf0ac3e3bfe8b7
Scott Giles <scottgiles@Scotts-MacBook-Pro.local>
2017-04-05 14:05:59 +0200

    First commit

---
+++ Board.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Scott Giles
+ */
+public class Board {
+
+	private int size; // The dimensions of the board - must be divisible by 3
+	private int difficulty;
+
+	public Board(int size) {
+		this.size = size;
+	}
+
+	public void populate(int difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public int getDiff() {
+		return difficulty;
+	}
+}
---
+++ Driver.java
@@ -0,0 +1,30 @@
+
+/*
+ *  Author: Scott Giles
+ *  This project is to get used to using GitHub again
+ */
+
+import java.util.*;
+import javax.swing.*;
+
+public class Driver extends JFrame {
+
+	public static void main(String[] args) {
+
+		int action = -1;
+		Scanner scan = new Scanner(System.in);
+		while (action != 0) {
+			System.out.println("Welcome to SodukoSolver!");
+			System.out.println("To generate a new board, enter 1.");
+			System.out.println("To quit, enter 0.");
+			action = scan.nextInt();
+			if (action == 1) {
+				System.out.println("Default size 9x9");
+				System.out.println("Default difficulty 4");
+				Board b = new Board(9);
+				b.populate(4);
+			}
+		}
+		scan.close();
+	}
+}
