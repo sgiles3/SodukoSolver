6304fa7fff4a8b4ce24d5dbba179e564abd57337
Scott Giles <sgiles3@u.rochester.edu>
2017-04-05 21:04:34 +0200

    Added Soduko.java

---
+++ Soduko.java
@@ -0,0 +1,58 @@
+/*
+ * Author: Scott Giles
+ * The code for the GUI window
+ */
+import java.awt.event.*;
+import javax.swing.JPanel;
+
+public class Soduko extends JPanel implements ActionListener, KeyListener, ComponentListener {
+
+	@Override
+	public void componentResized(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
