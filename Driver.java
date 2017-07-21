3cf299d2df29e2d74db808ac1823661ef647b2f6
Scott Giles <sgiles3@u.rochester.edu>
2017-07-21 18:30:53 -0400

    Test

--- Driver.java
+++ Driver.java
@@ -1,4 +1,32 @@
+/*
+ * Author: Scott Giles
+ * Text-only Driver (no GUI)
+ */
+
+import java.util.*;
 
 public class Driver {
-
+	
+	public static void main (String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int action = -1;
+		
+		// Main action loop (quit on exit)
+		while (action != 0) {
+			System.out.println("Welcome to SodukoSolver! (text-only)");
+			System.out.println("Main Menu");
+			System.out.println("To start a new puzzle, enter 1.");
+			System.out.println("To quit, enter 0.");
+			action = scan.nextInt();
+			
+			if (action == 1) {
+				TextBoard b = new TextBoard(9);
+				b.demo();
+				b.p();
+			}
+		}
+		scan.close();
+	}
 }
